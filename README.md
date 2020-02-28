3ba40a1c0ed71c1e8d015b187ab75f4096131f28
lcboy <lcboy@foxmail.com>
2020-02-28 16:43:26 +0800

    lcboy
    liu1989

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+# 二进制文件下载地址：[release](https://github.com/RManOfCN/crack-v2ray-sspanel-v3-mod_Uim-plugin/releases)
+
+
+
+
+
--- install-release.sh
+++ install-release.sh
@@ -164,7 +164,12 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
+<<<<<<< Updated upstream
     DOWNLOAD_LINK="https://github.com/rico93/pay-v2ray-sspanel-v3-mod_Uim-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+=======
+    DOWNLOAD_LINK="https://github.com/RManOfCN/pay-v2ray-sspanel-v3-mod_Uim-plugin/releases/download/crack/v2ray-linux-64_crack.zip"
+    colorEcho ${BLUE} ${DOWNLOAD_LINK}
+>>>>>>> Stashed changes
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
@@ -246,7 +251,11 @@ getVersion(){
         if [[ ${CUR_VER} != v* ]]; then
             CUR_VER=v${CUR_VER}
         fi
+<<<<<<< Updated upstream
         TAG_URL="https://api.github.com/repos/rico93/pay-v2ray-sspanel-v3-mod_Uim-plugin/releases/latest"
+=======
+        TAG_URL="https://api.github.com/repos/v2rayv3/pay-v2ray-sspanel-v3-mod_Uim-plugin/releases/latest"
+>>>>>>> Stashed changes
         NEW_VER=`curl ${PROXY} -s ${TAG_URL} --connect-timeout 10| grep 'tag_name' | cut -d\" -f4`
         if [[ ${NEW_VER} != v* ]]; then
           NEW_VER=v${NEW_VER}
